10e8daf5e5bb8d2569e9b7fde22d32e83aa8a8fd
DGUHJH <ghkdwogud852@gmail.com>
2024-03-08 13:24:10 +0900

    황재형 2020112034

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 name = input("이름 : ")
 unique_number = input("학번 : ")
+department = input("학과 : ")
 
-print(f'이름 : {name}, 학번 : {unique_number}')
+print(f'이름 : {name}, 학번 : {unique_number}, 학과 : {department}')
